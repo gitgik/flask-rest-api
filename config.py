42603011f7a319a154b81392e90f988d38c45d43
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-04-17 16:34:54 +0300

    [chore] define config for dev and production environments

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+import tempfile
+
+
+class BaseConfig(object):
+    """Base configuration class."""
+
+    DATABASE_URL = 'sqlite://:memory:'
+    SECRET = 'some-very-long-string-of-characters'
+
+
+class DevelopmentConfig(BaseConfig):
+    """This class represents configurations for Development."""
+
+    DEBUG = True
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    """This class represents configurations for Production."""
+
+    DEBUG = False
+    TESTING = False
