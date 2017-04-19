a75e91e1ca51fb25d594a01aeae24462d270639f
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-04-19 13:48:29 +0300

    [chore] set sql track modifications flag to false

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
     return app
