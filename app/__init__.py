e9579ef00645da1fbffd296e9e7cbb1e8007fb28
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-04-19 17:10:36 +0300

    [chore] remove unnecessary config lookup

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,6 @@ db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
