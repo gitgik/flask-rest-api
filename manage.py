e32d1518ebbb1ba43f3f7303a198982719c3f8c0
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-04-20 17:20:49 +0300

    [feature] add setup and teardown for testing

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 import os
-from flask_script import Manager # class for handling a set of commands
+# class for handling a set of commands
+from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
-from app import models
 
 
 app = create_app(config_name=os.getenv('APP_SETTINGS'))
---
+++ test_bucketlist.py
@@ -0,0 +1,26 @@
+import unittest
+from app import create_app, db
+
+
+class BucketlistTestCase(unittest.TestCase):
+    """This class represents the bucketlist test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app(config_name=os.getenv("APP_SETTINGS"))
+        # binds the app to the current context
+        with self.app.app_context():
+            # create all tables
+            db.create_all()
+
+    def tearDown(self):
+        """teardown all initialized variables."""
+        with self.app.app_context():
+            # drop all tables
+            db.session.remove()
+            db.drop_all()
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
