0c371a3b66aeaf248a27824370cc6311fd1028fd
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-04-20 19:25:43 +0300

    [chore] test api can interact with bucketlist

--- test_bucketlist.py
+++ test_bucketlist.py
@@ -9,6 +9,9 @@ class BucketlistTestCase(unittest.TestCase):
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = create_app(config_name=os.getenv("APP_SETTINGS"))
+        self.client = self.app.test_client
+        self.bucketlist = {'name': 'Go to Borabora for vacay'}
+
         # binds the app to the current context
         with self.app.app_context():
             # create all tables
@@ -16,19 +19,49 @@ class BucketlistTestCase(unittest.TestCase):
 
     def test_bucketlist_creation(self):
         """Test API can create a bucketlist (POST request)"""
-        pass
+        res = self.client().post('/bucketlists', data=self.bucketlist)
+        self.assertEqual(res.status_code, 201)
+        self.assertIn('Go to Borabora for vacay', res.data)
 
-    def test_bucketlist_retrieval(self):
+    def test_api_can_get_all_bucketlists(self):
         """Test API can get a bucketlist (GET request)."""
-        pass
+        res = self.client().get('/bucketlists')
+        self.assertEqual(res.status_code, 200)
+        self.assertIn('Go to Borabora for vacay', res.data)
+
+    def test_api_can_get_bucketlist_by_id(self):
+        """Test API can get a single bucketlist by using it's id."""
+        bid = 1
+        res = self.client().get('/bucketlists/{}/'.format(bid))
+        self.assertEqual(res.status_code, 200)
+        self.assertIn('Go to Borabora', res.data)
 
     def test_bucketlist_can_be_edited(self):
         """Test API can edit an existing bucketlist. (PUT request)"""
-        pass
+        rv = self.client().post(
+            '/bucketlists/',
+            data={'name': 'Eat, pray and love'})
+        self.assertEqual(rv.status_code, 201)
+        rv = self.client().put(
+            '/bucketlists/1',
+            data={
+                "name": "Dont just eat, but also pray and love :-)"
+            },
+            headers=headers)
+        self.assertEqual(rv.status_code, 200)
+        results = self.client().get('/bucketlists/1')
+        self.assertIn('Dont just eat', results.data)
 
     def test_bucketlist_deletion(self):
         """Test API can delete an existing bucketlist. (DELETE request)."""
-        pass
+        rv = self.client().post(
+            '/bucketlists/',
+            data={'name': 'Eat, pray and love'})
+        self.assertEqual(rv.status_code, 201)
+        rv = self.client().delete('/bucketlists/1')
+        self.assertEqual(rv.status_code, 200)
+        results = self.client().get('/bucketlists/1')
+        self.assertIn('No such bucketlist', results.data)
 
     def tearDown(self):
         """teardown all initialized variables."""
