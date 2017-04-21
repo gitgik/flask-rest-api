e3cf139196ef93bbe49c182dc679cfacf3d5384a
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-04-21 17:14:36 +0300

    [feature] add timestamps, save and delete methods on bucketlist model

--- app/models.py
+++ app/models.py
@@ -8,6 +8,22 @@ class Bucketlist(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+
+    def __init__(self, name):
+        """initialize with name."""
+        self.name = name
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
     def __repr__(self):
         return "<Bucketlist: {}>".format(self.name)
