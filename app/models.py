04c9a1b6b99f82576578dad38aacf806c353f307
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-04-28 23:07:51 +0300

    [fix] relationship argument cascade in case of user deletion

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(db.Model):
     username = db.Column(db.String(256), nullable=False, unique=True)
     password = db.Column(db.String(256))
     bucketlists = db.relationship(
-        'Bucketlist', order_by='Bucketlist.id' cascade="all, delete-orphan")
+        'Bucketlist', order_by='Bucketlist.id', cascade="all, delete-orphan")
 
     def __init__(self, username, password):
         self.username = username
@@ -34,6 +34,7 @@ class Bucketlist(db.Model):
         db.DateTime, default=db.func.current_timestamp(),
         onupdate=db.func.current_timestamp())
     created_by = db.Column(db.Integer, db.ForeignKey(User.id))
+
 
     def __init__(self, name):
         """initialize with name."""
