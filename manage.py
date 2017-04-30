4bb6d03f72acc87dc6a40fbbae3398319ac7db2b
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-04-30 16:26:12 +0300

    [chore] automate test using test command in manage.py, create test folder for tests

--- manage.py
+++ manage.py
@@ -1,15 +1,25 @@
 import os
+import unittest
 # class for handling a set of commands
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
-
 
 app = create_app(config_name=os.getenv('APP_SETTINGS'))
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 
 if __name__ == '__main__':
