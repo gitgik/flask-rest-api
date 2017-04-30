3195666f651400ffe8783c7981627132ee1b408c
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-04-30 23:45:56 +0300

    [chore] add login functionality, define url for login

--- app/auth/views.py
+++ app/auth/views.py
@@ -39,11 +39,48 @@ class RegistrationView(MethodView):
 
             return make_response(jsonify(response)), 202
 
+
+class LoginView(MethodView):
+    """This class-based view handles user login and access token generation."""
+
+    def post(self):
+        try:
+            user = User.query.filter_by(email=request.data['email']).first()
+
+            if user and user.password_is_valid(request.data['password']):
+                # Generate the access token
+                access_token = user.generate_token(user.id)
+                if access_token:
+                    response = {
+                        'message': 'You logged in successfully.',
+                        'access_token': access_token.decode()
+                    }
+                    return make_response(jsonify(response)), 200
+            else:
+                # User does not exist
+                response = {
+                    'message': 'Invalid email or password, Please try again'
+                }
+                return make_response(jsonify(response)), 401
+
+        except Exception as e:
+            response = {
+                'message': str(e)
+            }
+            return make_response(jsonify(response)), 500
+
+
 # Define the API resource
 registration_view = RegistrationView.as_view('registration_view')
+login_view = LoginView.as_view('login_view')
 
 # Add the url rule for registering a user
 auth_blueprint.add_url_rule(
     '/auth/register',
     view_func=registration_view,
     methods=['POST'])
+auth_blueprint.add_url_rule(
+    '/auth/login',
+    view_func=login_view,
+    methods=['POST']
+)
