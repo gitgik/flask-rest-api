5a59d64a2490de31ec4e670869208e5311544e4e
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-05-08 22:03:06 +0300

    [chore] document manage.py

--- manage.py
+++ manage.py
@@ -5,13 +5,19 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
 
+# initialize the app with all its configurations
 app = create_app(config_name=os.getenv('APP_SETTINGS'))
 migrate = Migrate(app, db)
+# create an instance of class that will handle our commands
 manager = Manager(app)
 
+# Define the migration command to always be preceded by the word "db"
+# Example usage: python manage.py db init
 manager.add_command('db', MigrateCommand)
 
 
+# define our command for testing called "test"
+# Usage: python manage.py test
 @manager.command
 def test():
     """Runs the unit tests without test coverage."""
