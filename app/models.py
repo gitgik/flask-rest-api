30c8c5e62126fc6e29e8f19a4a12f84646a61cc0
Jee Githinji Gikera <githinjigikera@gmail.com>
2017-05-08 21:30:17 +0300

    [chore] document models.py

--- app/models.py
+++ app/models.py
@@ -6,10 +6,11 @@ from datetime import datetime, timedelta
 
 
 class User(db.Model):
-    """Maps to users table """
+    """This class defines the users table """
 
     __tablename__ = 'users'
 
+    # Define the columns of the users table, starting with the primary key
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(256), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
@@ -17,26 +18,32 @@ class User(db.Model):
         'Bucketlist', order_by='Bucketlist.id', cascade="all, delete-orphan")
 
     def __init__(self, email, password):
+        """Initialize the user with an email and a password."""
         self.email = email
         self.password = Bcrypt().generate_password_hash(password).decode()
 
     def password_is_valid(self, password):
-        """Validates user password """
+        """Checks the password against it's hash to validates the user's password"""
         return Bcrypt().check_password_hash(self.password, password)
 
     def save(self):
+        """Save a user to the database.
+        This includes creating a new user and editing one.
+        """
         db.session.add(self)
         db.session.commit()
 
     def generate_token(self, user_id):
-        """ Generates the access token"""
+        """Generates the access token to be used as the Authorization header"""
 
         try:
+            # set up a payload with an expiration time
             payload = {
                 'exp': datetime.utcnow() + timedelta(minutes=5),
                 'iat': datetime.utcnow(),
                 'sub': user_id
             }
+            # encode the payload to get an byte string token
             jwt_string = jwt.encode(
                 payload,
                 current_app.config.get('SECRET'),
@@ -45,6 +52,7 @@ class User(db.Model):
             return jwt_string
 
         except Exception as e:
+            # return an error in string format if an exception occurs
             return str(e)
 
     @staticmethod
@@ -60,7 +68,7 @@ class User(db.Model):
 
 
 class Bucketlist(db.Model):
-    """This class represents the bucketlist table."""
+    """This class defines the bucketlist table."""
 
     __tablename__ = 'bucketlists'
 
