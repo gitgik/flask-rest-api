dc7044d84f70789b126a3dbe45cb7aedf263dbca
Jee Gikera <jeegiks@gmail.com>
2022-01-22 11:59:15 +0300

    fix access token login issue

--- app/auth/views.py
+++ app/auth/views.py
@@ -52,7 +52,7 @@ class LoginView(MethodView):
                 if access_token:
                     response = {
                         'message': 'You logged in successfully.',
-                        'access_token': access_token.decode()
+                        'access_token': access_token
                     }
                     return make_response(jsonify(response)), 200
             else:
