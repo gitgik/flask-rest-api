15e244adf952005fd6b86c570b46dc63848ac56e
Jee Gikera <jeegiks@gmail.com>
2022-01-22 10:54:57 +0300

    update deps, cleanup manage.py

--- manage.py
+++ manage.py
@@ -2,22 +2,18 @@ import os
 import unittest
 # class for handling a set of commands
 from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
 
 # initialize the app with all its configurations
 app = create_app(config_name=os.getenv('APP_SETTINGS'))
-migrate = Migrate(app, db)
+
 # create an instance of class that will handle our commands
 manager = Manager(app)
 
-# Define the migration command to always be preceded by the word "db"
-# Example usage: python manage.py db init
-manager.add_command('db', MigrateCommand)
-
-
 # define our command for testing called "test"
 # Usage: python manage.py test
+
+
 @manager.command
 def test():
     """Runs the unit tests without test coverage."""
